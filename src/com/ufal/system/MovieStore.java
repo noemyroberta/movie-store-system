17753be7891c508de3e8542be455bc76d52c5207
Noemy Roberta <noemy.oliveira2@outlook.com>
2023-04-23 12:49:19 -0300

    feat(main): get clients list from database

--- src/com/ufal/system/MovieStore.java
+++ src/com/ufal/system/MovieStore.java
@@ -1,11 +1,16 @@
 package src.com.ufal.system;
 
+import src.com.ufal.system.beans.Client;
+
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MovieStore {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         DBConnection.start();
-
+        ArrayList<Client> clients = DBConnection.getClients();
+        System.out.println(clients.toString());
+        DBConnection.close();
     }
 }
