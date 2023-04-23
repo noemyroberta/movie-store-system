628a45fa7da4e54810f57ed145c7ac220d649b53
Noemy Roberta <noemy.oliveira2@outlook.com>
2023-04-23 12:41:17 -0300

    refactor: position connection attributes outside `start()` method

--- src/com/ufal/system/DBConnection.java
+++ src/com/ufal/system/DBConnection.java
@@ -6,17 +6,15 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class DBConnection {
+    static final String USER = "localhost";
+    static final String PASSWORD = "";
+    static final String PORT = "3306";
+    static final String DATABASE = "db_movie_store";
+    static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String URL = "jdbc:mysql://" + USER + ":" + PORT + "/" + DATABASE;
+    static Connection connection;
 
     static void start() throws ClassNotFoundException, SQLException {
-        final String USER = "localhost";
-        final String PASSWORD = "";
-
-        final String PORT = "3306";
-        final String DATABASE = "db_movie_store";
-        final String DRIVER = "com.mysql.cj.jdbc.Driver";
-        final String URL = "jdbc:mysql://"+USER+":"+PORT+"/"+DATABASE;
-        Connection connection;
-
         Class.forName(DRIVER);
         connection = (Connection) DriverManager.getConnection(URL, USER, PASSWORD);
         connection.close();
